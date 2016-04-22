08c66a2f6ad7b86875fad7ec0ae0213e81099542
wolftic <lorenzokramertje@gmail.com>
2016-04-22 13:44:54 +0200

    Comet

--- Assets/Scripts/Comet.cs
+++ Assets/Scripts/Comet.cs
@@ -2,48 +2,26 @@
 using System.Collections;
 
 public class Comet : MonoBehaviour {
-
-	public enum CometType
-	{
-		BIG,
-		MIDDLE,
-		SMALL
-	}
-
-	public CometType cometType;
+	bool isSinking = false;
 
 	void Start () {
-		transform.tag = "Comet";
-		UpdateScale ();
+		Invoke ("Sink", 1.0f);
 	}
 
-	void Update() {
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			Split ();
-		}
+	float percentage = 0;
 
-		transform.Translate (Vector3.up * Time.deltaTime);
-	}
-
-	public void UpdateScale () {
-		if (cometType == CometType.BIG) {
-			transform.localScale = new Vector2 (2, 2);
-		} else if(cometType == CometType.MIDDLE) {
-			transform.localScale = new Vector2 (1, 1);
-		} else {
-			transform.localScale = new Vector2 (.5f, .5f);
-		}
-	}
-
-	public void Split () {
-		if (cometType != CometType.SMALL) {
-			for(int i = 0; i < 2; i++) {
-				Comet comet = Instantiate (Resources.Load ("Comets/Comet", typeof(Comet)), transform.position, Quaternion.Euler (0, 0, Random.Range (0, 360))) as Comet;
-				comet.cometType = (cometType == CometType.BIG) ? CometType.MIDDLE : CometType.SMALL;
-				comet.UpdateScale ();
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime);
+		if (isSinking) {
+			percentage += 0.1f * Time.deltaTime;
+			transform.localScale = Vector3.Lerp (transform.localScale, Vector3.zero, percentage);
+			if (percentage > 1) {
+				Destroy (gameObject);
 			}
 		}
+	}	
 
-		Destroy (gameObject);
+	void Sink () {
+		isSinking = true;
 	}
 }
