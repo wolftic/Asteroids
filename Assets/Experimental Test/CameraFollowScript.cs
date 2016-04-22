109061303965758110ba90cff4ec6087dbbc96e6
wolftic <lorenzokramertje@gmail.com>
2016-04-22 08:48:21 +0200

    Experimental

---
+++ Assets/Experimental Test/CameraFollowScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour {
+	
+	public Transform player;
+	Vector3 offset;
+
+	void Start () {
+		offset = transform.position;
+	}
+
+	void Update () {
+		transform.position = Vector3.Lerp (transform.position, player.position + offset, 5f * Time.deltaTime);
+	}
+}
---
+++ Assets/Experimental Test/Movement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float xSpeed;
+
+	Vector3 movement;
+	public ParticleSystem[] thrusters;
+
+	void Start () {
+		
+	}
+
+	Quaternion newRot;
+
+	void Update () {
+
+		if(Input.GetAxisRaw("Vertical") != 0) {
+			for (int i = 0; i < thrusters.Length; i++) {
+				if (!thrusters [i].isPlaying) {
+					thrusters [i].Play ();
+				}
+			}
+			xSpeed += Input.GetAxis ("Vertical");
+			xSpeed = Mathf.Clamp (xSpeed, -10f, 10f);
+		} else {
+			for (int i = 0; i < thrusters.Length; i++) {
+				if (thrusters [i].isPlaying) {
+					thrusters [i].Stop ();
+				}
+			}
+			xSpeed *= 0.99f;
+		}
+
+		float horizontal = Input.GetAxis ("Horizontal");
+		newRot = Quaternion.Euler(-horizontal * 30.0f * Vector3.forward);;
+
+		transform.Translate (Vector3.forward * Time.deltaTime * xSpeed);
+ 		transform.Rotate (horizontal * 1.0f * Vector3.up);
+		transform.GetChild (0).localRotation = Quaternion.Lerp (transform.GetChild (0).localRotation, newRot, 5.0f * Time.deltaTime);
+	}
+}
