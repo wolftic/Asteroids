a473ed1292a4c974f966005cf70a34b969aab533
jvdwijk <joeyvdwijk@gmail.com>
2016-04-22 14:59:22 +0200

    patch #1

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,6 +5,9 @@ public class PlayerHealth : MonoBehaviour
 {
 	public float Health;
 
+	private float regenDelay = 1.0F;
+	private float regenTime = 0.0F;
+
 	public void doDamage(float damage) 
 	{
 		Health -= damage;
@@ -12,10 +15,21 @@ public class PlayerHealth : MonoBehaviour
 
 	void Update()
 	{
+		if(Time.time > regenTime)
+		{
+			Regeneration ();
+		}
+
 		if(Health <= 0)
 		{
 			Destroy (gameObject);
 
 		}
+	}
+
+	private void Regeneration()
+	{
+		Health++;
+		regenTime = Time.time + regenDelay;
 	}
 }
