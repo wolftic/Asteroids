91e6054f206a8ea8d6b7dbf836cb027d86ad0c27
jvdwijk <joeyvdwijk@gmail.com>
2016-04-26 13:54:30 +0200

    Shop Menu

---
+++ Assets/ShopMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopMenuScript : MonoBehaviour {
+
+	public Canvas buyMenu;
+	public Canvas sellMenu;
+
+	void Awake () {
+	
+		buyMenu = buyMenu.GetComponent <Canvas> ();
+		sellMenu = sellMenu.GetComponent <Canvas> ();
+
+		buyMenu.enabled = true;
+		sellMenu.enabled = false;
+	}
+
+	public void SelectBuyMenu()
+	{
+		buyMenu.enabled = true;
+		sellMenu.enabled = false;
+	}
+
+	public void SelectSellMenu()
+	{
+		buyMenu.enabled = false;
+		sellMenu.enabled = true;
+	}
+}
