64d1be27f242fb4aeabda820d99610b9af94c7a3
wolftic <lorenzokramertje@gmail.com>
2016-04-26 11:34:01 +0200

    Error fix

--- Assets/Scripts/Comet.cs
+++ Assets/Scripts/Comet.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class Comet : MonoBehaviour {
 	bool isSinking = false;
+	float time;
 
 	void Start () {
-		Invoke ("Sink", 10.0f);
+		time = Time.time;
 	}
 
 	float percentage = 0;
 
 	void Update () {
 		transform.Translate (Vector3.forward * 10.0f * Time.deltaTime);
+		if (!isSinking && time < Time.time) {
+			time = Time.time + 5f;
+			isSinking = true;
+		}
 		if (isSinking) {
 			percentage += 0.1f * Time.deltaTime;
 			transform.localScale = Vector3.Lerp (transform.localScale, Vector3.zero, percentage);
@@ -27,10 +32,14 @@ public class Comet : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Player") {
-			other.GetComponent<PlayerHealth> ().doDamage (10f);
+			other.GetComponent<PlayerHealth> ().doDamage (25f);
 		}
 		if (other.tag == "Enemy") {
-			other.GetComponent<EnemyHealth> ().doDamage (10f);
+			other.GetComponent<EnemyHealth> ().doDamage (25f);
 		}
+	}
+
+	public void split() {
+
 	}
 }
--- Assets/Scripts/CometSpawner.cs
+++ Assets/Scripts/CometSpawner.cs
@@ -2,14 +2,31 @@
 using System.Collections;
 
 public class CometSpawner : MonoBehaviour {
+	public Transform comet;
+	public float spawnDelay;
+	public float radius;
+	private float spawnTime;
 
-	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		if (spawnTime < Time.time) {
+			spawnTime = Time.time + spawnDelay;
+			//
+
+			Vector3 random = Random.insideUnitSphere;
+			random.y = 0;
+			Vector3 position = random * radius;
+
+			Quaternion rotation = Quaternion.Euler (0, Random.Range (0, 359), 0);
+
+			SpawnComet(position, rotation);
+		}
+	}
+
+	void SpawnComet (Vector3 position, Quaternion rotation) {
+		Instantiate (comet, position, rotation);
 	}
 }
