53dbe398f0fec17e855b8c7a00816be5738247b7
jvdwijk <joeyvdwijk@gmail.com>
2016-05-10 16:27:06 +0200

    update
    Menu UI update
    Input handler begin

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InputHandler : MonoBehaviour {
+
+
+	Dictionary<string, KeyCode> inputs = new Dictionary<string, KeyCode> ();
+
+	void Start(){
+		inputs.Add (camLeft, KeyCode.Q);
+		inputs.Add (camRight, KeyCode.E);
+		inputs.Add (pauzeKnop, KeyCode.Escape);
+		inputs.Add (shootKnop, KeyCode.Space);
+		inputs.Add (reloadKnop, KeyCode.R);
+		inputs.Add (forwardKnop, KeyCode.W);
+		inputs.Add (leftKnop, KeyCode.A);
+		inputs.Add (downKnop, KeyCode.S);
+		inputs.Add (rightKnop, KeyCode.D);
+	}
+}
