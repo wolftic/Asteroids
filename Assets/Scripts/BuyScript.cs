975f9476fd4cbdf2d5fd623dca5e1aacfae086af
jvdwijk <joeyvdwijk@gmail.com>
2016-05-10 13:07:59 +0200

    Shop Update #10
    Script verbeterd

--- Assets/Scripts/BuyScript.cs
+++ Assets/Scripts/BuyScript.cs
@@ -5,263 +5,275 @@ using ItemType = PlayerInventory.ItemType;
 public class BuyScript : MonoBehaviour {
 
 	private PlayerInventory playerInventory;
+	[HideInInspector]
+	public int sellButton;
+	[SerializeField]
+	private int buyButton;
 
 	void Awake()
 	{
 		playerInventory = GameObject.FindGameObjectWithTag ("Playerinventory").GetComponent <PlayerInventory>();
 	}
 
-	//buy functions
-
-	//koop functie aanmaken
-	public void BuySSEngalica()
+	//shop functie aanmaken
+	public void ShopInteraction(GameObject knop)
 	{
-		//als item niet in inventory zit kan jij code uitvoeren
-		if(playerInventory.InInventory (ItemType.SSEngalica) == false)
-		{
-			//als jij zoveel coins heeft kan jij code uitvoeren
-			if(playerInventory.DeductCoins (20))
+		switch (knop.tag) {
+
+		case "Buyknop5":
+			//als item niet in inventory zit kan jij code uitvoeren
+			if(playerInventory.InInventory (ItemType.SSEngalica) == false)
 			{
-				//Item in inventory toevoegen
-				playerInventory.AddItem (ItemType.SSEngalica);
-				//aangeven dat speler item gekocht heeft.
-				Debug.Log ("Bought SSEngalica");
+				//als jij zoveel coins heeft kan jij code uitvoeren
+				if(playerInventory.DeductCoins (20))
+				{
+					//Item in inventory toevoegen
+					playerInventory.AddItem (ItemType.SSEngalica);
+					//aangeven dat speler item gekocht heeft.
+					Debug.Log ("Bought SSEngalica");
+				}
+				//als speler te weinig geld heeft
+				else
+				{
+					//aangeven dat speler te weinig geld heeft.
+					Debug.Log ("Get More moneyz");
+				}
 			}
-			//als speler te weinig geld heeft
+			//als je item al hebt.
 			else
-			{
-				//aangeven dat speler te weinig geld heeft.
-				Debug.Log ("Get More moneyz");
-			}
-		}
-		//als je item al hebt.
-		else
-			//aangeven dat speler het item al heeft.
-			Debug.Log ("Already got it!");
-	}
+				//aangeven dat speler het item al heeft.
+				Debug.Log ("Already got it!");
 
-	public void BuySCStriker()
-	{
-		if (playerInventory.InInventory (ItemType.SCStriker) == false)
+			break;
+
+		case "Buyknop6":
+			if (playerInventory.InInventory (ItemType.SCStriker) == false)
 			if (playerInventory.DeductCoins (20)) {
 				playerInventory.AddItem (ItemType.SCStriker);
 			} else
 				Debug.Log ("Get More Moneyz");
 			else
 				Debug.Log ("Already got it");
-	}
 
-	public void BuyGun1()
-	{
-		if(playerInventory.InInventory (ItemType.Gun1) == false)
-		{
-			if(playerInventory.DeductCoins (20))
+			break;
+
+		case "Buyknop3":
+			if(playerInventory.InInventory (ItemType.Gun1) == false)
 			{
-				playerInventory.AddItem (ItemType.Gun1);
-				Debug.Log ("Bought Gun1");
+				if(playerInventory.DeductCoins (20))
+				{
+					playerInventory.AddItem (ItemType.Gun1);
+					Debug.Log ("Bought Gun1");
+				}
+				else
+				{
+					Debug.Log ("Get More moneyz");
+				}
 			}
 			else
-			{
-				Debug.Log ("Get More moneyz");
-			}
-		}
-		else
-			Debug.Log ("Already got it!");
-	}
+				Debug.Log ("Already got it!");
 
-	public void BuyAmmoGun1()
-	{
-		if (playerInventory.DeductCoins (10)) {
-			playerInventory.AddItem (ItemType.AmmoGun1);
-			Debug.Log ("bought ammo");
-		} else
-			Debug.Log ("get more money");
-	}
+			break;
 
-	public void BuyGun2()
-	{
-		if(playerInventory.InInventory (ItemType.Gun2) == false)
-		{
-			if(playerInventory.DeductCoins (20))
-			{
-				playerInventory.AddItem (ItemType.Gun2);
-				Debug.Log ("Bought Gun2");
-			}
-			else
-			{
-				Debug.Log ("Get More moneyz");
-			}
-		}
-		else
-			Debug.Log ("Already got it!");
-	}
 
-	public void BuyAmmoGun2()
-	{
-		if (playerInventory.DeductCoins (10)) {
-			playerInventory.AddItem (ItemType.AmmoGun2);
-			Debug.Log ("bought ammo");
-		} else
-			Debug.Log ("get more money");
-		}
+		case "Buyknop1":
+			if (playerInventory.DeductCoins (10)) {
+				playerInventory.AddItem (ItemType.AmmoGun1);
+				Debug.Log ("bought ammo");
+			} else
+				Debug.Log ("get more money");
 
-	//koop upgrade functie aanmaken
-	public void BuyUpgradeSpeed()
-	{
-		//als speler nog geen upgrade heeft gekocht voer code uit
-		if(playerInventory.InInventory (ItemType.SpeedUpgrade) == false)
-		{
-			//test of de speler genoeg coins heb en haal het geld weg, voer daarna code uit
-			if(playerInventory.DeductCoins (20))
-			{
-				//add upgrade aan inventory
-				playerInventory.AddItem (ItemType.SpeedUpgrade);
-				//aangeven dat de speler upgrade gekocht heeft
-				Debug.Log ("Bought speed upgrade");
-			}
-			//als speler te weinig geld heeft voer code uit
-			else
-			{
-				//aangeven dat speler te weinig geld heeft
-				Debug.Log ("Get More moneyz");
-			}
-		}
-		//als speler upgrade al heeft
-		else
-			//aangeven dat speler upgrade al heeft
-			Debug.Log ("Already got it!");
-	}
+			break;
 
-	public void BuyUpgradeHealth()
-	{
-		if(playerInventory.InInventory (ItemType.HealthUpgrade) == false)
-		{
-			if(playerInventory.DeductCoins (20))
+		case "Buyknop4":
+			if(playerInventory.InInventory (ItemType.Gun2) == false)
 			{
-				playerInventory.AddItem (ItemType.HealthUpgrade);
-				Debug.Log ("Bought health upgrade");
+				if(playerInventory.DeductCoins (20))
+				{
+					playerInventory.AddItem (ItemType.Gun2);
+					Debug.Log ("Bought Gun2");
+				}
+				else
+				{
+					Debug.Log ("Get More moneyz");
+				}
 			}
 			else
-			{
-				Debug.Log ("Get More moneyz");
-			}
-		}
-		else
-			Debug.Log ("Already got it!");
-	}
+				Debug.Log ("Already got it!");
 
-	public void BuyUpgradeFirepower()
-	{
-		if(playerInventory.InInventory (ItemType.FirePowerUpgrade) == false)
-		{
-			if(playerInventory.DeductCoins (20))
+			break;
+
+		case "Buyknop2":
+			if (playerInventory.DeductCoins (10)) {
+				playerInventory.AddItem (ItemType.AmmoGun2);
+				Debug.Log ("bought ammo");
+			} else
+				Debug.Log ("get more money");
+
+			break;
+
+		case "Buyknop7":
+			//als speler nog geen upgrade heeft gekocht voer code uit
+			if(playerInventory.InInventory (ItemType.SpeedUpgrade) == false)
 			{
-				playerInventory.AddItem (ItemType.FirePowerUpgrade);
-				Debug.Log ("Bought firepower upgrade");
+				//test of de speler genoeg coins heb en haal het geld weg, voer daarna code uit
+				if(playerInventory.DeductCoins (20))
+				{
+					//add upgrade aan inventory
+					playerInventory.AddItem (ItemType.SpeedUpgrade);
+					//aangeven dat de speler upgrade gekocht heeft
+					Debug.Log ("Bought speed upgrade");
+				}
+				//als speler te weinig geld heeft voer code uit
+				else
+				{
+					//aangeven dat speler te weinig geld heeft
+					Debug.Log ("Get More moneyz");
+				}
+			}
+			//als speler upgrade al heeft
+			else
+				//aangeven dat speler upgrade al heeft
+				Debug.Log ("Already got it!");
+
+			break;
+
+		case "Buyknop8":
+			if(playerInventory.InInventory (ItemType.HealthUpgrade) == false)
+			{
+				if(playerInventory.DeductCoins (20))
+				{
+					playerInventory.AddItem (ItemType.HealthUpgrade);
+					Debug.Log ("Bought health upgrade");
+				}
+				else
+				{
+					Debug.Log ("Get More moneyz");
+				}
 			}
 			else
+				Debug.Log ("Already got it!");
+
+			break;
+
+		case "Buyknop9":
+			if(playerInventory.InInventory (ItemType.FirePowerUpgrade) == false)
 			{
-				Debug.Log ("Get More moneyz");
+				if(playerInventory.DeductCoins (20))
+				{
+					playerInventory.AddItem (ItemType.FirePowerUpgrade);
+					Debug.Log ("Bought firepower upgrade");
+				}
+				else
+				{
+					Debug.Log ("Get More moneyz");
+				}
 			}
-		}
-		else
-			Debug.Log ("Already got it!");
-	}
+			else
+				Debug.Log ("Already got it!");
 
-	//sell functions
+			break;
 
-	//verkoop functie aanmaken
-	public void SellAmmoGun1()
-	{
-		//test of item in inventory zit en haal item weg, voer daarna de code uit
-		if (playerInventory.DeductItem (ItemType.AmmoGun1)) {
-			//geef coins aan speler
-			playerInventory.coins += 10;
-			//aangeven dat item verkocht is
-			Debug.Log ("sold ammo");
-		} else
-			//aangeven dat speler het item niet heeft
-			Debug.Log ("Niks om te verkopen");
-	}
+		case "Sellknop1":
+			if (playerInventory.DeductItem (ItemType.AmmoGun1)) {
+				//geef coins aan speler
+				playerInventory.coins += 10;
+				//aangeven dat item verkocht is
+				Debug.Log ("sold ammo");
+			} else
+				//aangeven dat speler het item niet heeft
+				Debug.Log ("Niks om te verkopen");
+				
+			break;
 
-	public void SellAmmoGun2()
-	{
-		if(playerInventory.DeductItem (ItemType.AmmoGun2))
-		{
-			playerInventory.coins += 10;
-			Debug.Log ("sold ammo");
-		}
-	}
+		case "Sellknop2":
+			if (playerInventory.DeductItem (ItemType.AmmoGun2)) {
+				playerInventory.coins += 10;
+				Debug.Log ("sold ammo");
+			} else
+				Debug.Log ("Niks om te verkopen");
 
-	public void SellEnemyScrap()
-	{
-		if(playerInventory.DeductItem (ItemType.EnemyScrap))
-		{
-			playerInventory.coins += 10;
-			Debug.Log ("sold enemy scrap");
-		}
-	}
+			break;
 
-	public void SellCometScrap()
-	{
-		if(playerInventory.DeductItem (ItemType.CometScrap))
-		{
-			playerInventory.coins += 10;
-			Debug.Log ("sold Comet scrap");
-		}
-	}
+		case "Sellknop3":
+			if(playerInventory.DeductItem (ItemType.EnemyScrap))
+			{
+				playerInventory.coins += 10;
+				Debug.Log ("sold enemy scrap");
+			}else
+				Debug.Log ("Niks om te verkopen");
 
-	public void SellStars()
-	{
-		if(playerInventory.DeductItem (ItemType.Stars))
-		{
-			playerInventory.coins += 10;
-			Debug.Log ("sold stars");
-		}
-	}
+			break;
 
-	public void SellRustyPlates()
-	{
-		if(playerInventory.DeductItem (ItemType.RustySteelPlates))
-		{
-			playerInventory.coins += 10;
-			Debug.Log ("sold Rusty Steel Plates");
-		}
-	}
+		case "Sellknop4":
+			if(playerInventory.DeductItem (ItemType.CometScrap))
+			{
+				playerInventory.coins += 10;
+				Debug.Log ("sold Comet scrap");
+			}else
+				Debug.Log ("Niks om te verkopen");
 
-	public void SellGlass()
-	{
-		if(playerInventory.DeductItem (ItemType.Glass))
-		{
-			playerInventory.coins += 10;
-			Debug.Log ("sold Glass");
-		}
-	}
+			break;
 
-	public void SellMinerals()
-	{
-		if(playerInventory.DeductItem (ItemType.Minerals))
-		{
-			playerInventory.coins += 10;
-			Debug.Log ("sold Minerals");
-		}
-	}
+		case "Sellknop5":
+			if(playerInventory.DeductItem (ItemType.Stars))
+			{
+				playerInventory.coins += 10;
+				Debug.Log ("sold stars");
+			}else
+				Debug.Log ("Niks om te verkopen");
 
-	public void SellBrokenGuns()
-	{
-		if(playerInventory.DeductItem (ItemType.BrokenGuns))
-		{
-			playerInventory.coins += 10;
-			Debug.Log ("sold Broken Guns");
-		}
-	}
+			break;
 
-	public void SellBolts()
-	{
-		if(playerInventory.DeductItem (ItemType.Bolts))
-		{
-			playerInventory.coins += 10;
-			Debug.Log ("sold Bolts");
+		case "Sellknop6":
+			if(playerInventory.DeductItem (ItemType.RustySteelPlates))
+			{
+				playerInventory.coins += 10;
+				Debug.Log ("sold Rusty Steel Plates");
+			}else
+				Debug.Log ("Niks om te verkopen");
+
+			break;
+
+		case "Sellknop7":
+			if(playerInventory.DeductItem (ItemType.Glass))
+			{
+				playerInventory.coins += 10;
+				Debug.Log ("sold Glass");
+			}else
+				Debug.Log ("Niks om te verkopen");
+
+			break;
+
+		case "Sellknop8":
+			if(playerInventory.DeductItem (ItemType.Minerals))
+			{
+				playerInventory.coins += 10;
+				Debug.Log ("sold Minerals");
+			}else
+				Debug.Log ("Niks om te verkopen");
+
+			break;
+
+		case "Sellknop9":
+			if(playerInventory.DeductItem (ItemType.BrokenGuns))
+			{
+				playerInventory.coins += 10;
+				Debug.Log ("sold Broken Guns");
+			}else
+				Debug.Log ("Niks om te verkopen");
+
+			break;
+
+		case "Sellknop10":
+			if(playerInventory.DeductItem (ItemType.Bolts))
+			{
+				playerInventory.coins += 10;
+				Debug.Log ("sold Bolts");
+			}else
+				Debug.Log ("Niks om te verkopen");
+
+			break;
 		}
 	}
 }
--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -4,31 +4,45 @@ using System.Collections.Generic;
 
 public class PlayerInventory : MonoBehaviour {
 
+	//lijst met items aanmaken
 	public enum ItemType { SSEngalica, SCStriker, Gun1, AmmoGun1, Gun2, AmmoGun2, SpeedUpgrade, HealthUpgrade, FirePowerUpgrade, EnemyScrap, CometScrap, BrokenGuns, Bolts, 
 		Minerals, Glass, RustySteelPlates, Stars};
+	//dictionary voor inventory aanmaken
 	public Dictionary<ItemType, int> inventory = new Dictionary<ItemType, int>();
 
+	//de variabelen niet in de inspector laten zien
 	[HideInInspector]
+	//variabelen coins aanmaken
 	public int coins = 0;
 
+	//functie start aanmaken
 	void Start()
 	{
+		//niet verwijderen als scene geladen wordt.
 		DontDestroyOnLoad (gameObject);
+		//functie na een seconde starten (debug command)
 		Invoke ("CoinsDebug", 1f);
 	}
 
+	//functie voor debuggen aanmaken
 	void CoinsDebug()
 	{
-		
 		Debug.Log (coins);
+		//coins toevoegen
 		coins += 1;
+		//functie herhalen
 		Invoke ("CoinsDebug", 0.5f);
 	}
 
+	//boolean aanmaken met variabelen amount erin
 	public bool DeductCoins(int amount) {
+		//als jij meer coins dan amount hebt voer code uit
 		if (this.coins >= amount) {
+			//haal het amount van jouw coins af
 			coins -= amount;
+			//boolean op true zetten
 			return true;
+			//als jij te weinig coins hebt boolean op false zetten
 		} else return false;
 	}
 
@@ -39,17 +53,22 @@ public class PlayerInventory : MonoBehaviour {
 		} else return false;
 	}
 
+	//boolean aanmaken met itemType naam erin
 	public bool InInventory(ItemType name)
 	{
+		//als er in de inventory een item met de gegeven naam zit voer dan de code uit
 		if(inventory.ContainsKey (name) )
 		{
+			//als het item meer dan 1 keer in de inventory zit zet boolean op true.
 			if (inventory[name] > 0)
 				return true;
 		}
 		return false;
 	}
 
+	//functie AddItem aanmaken met ItemType name
 	public void AddItem(ItemType name) {
+		//private functie starten.
 		AddItem (name, 1);
 	}
 
