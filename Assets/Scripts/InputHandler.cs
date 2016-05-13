c1834b95bdcaff5f3c04bd20067e62ee7b02ce77
jvdwijk <joeyvdwijk@gmail.com>
2016-05-13 08:56:29 +0200

    update inputhandler

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -8,14 +8,14 @@ public class InputHandler : MonoBehaviour {
 	Dictionary<string, KeyCode> inputs = new Dictionary<string, KeyCode> ();
 
 	void Start(){
-		inputs.Add (camLeft, KeyCode.Q);
-		inputs.Add (camRight, KeyCode.E);
-		inputs.Add (pauzeKnop, KeyCode.Escape);
-		inputs.Add (shootKnop, KeyCode.Space);
-		inputs.Add (reloadKnop, KeyCode.R);
-		inputs.Add (forwardKnop, KeyCode.W);
-		inputs.Add (leftKnop, KeyCode.A);
-		inputs.Add (downKnop, KeyCode.S);
-		inputs.Add (rightKnop, KeyCode.D);
+		inputs.Add ("camLeft", KeyCode.Q);
+		inputs.Add ("camRight", KeyCode.E);
+		inputs.Add ("pauzeKnop", KeyCode.Escape);
+		inputs.Add ("shootKnop", KeyCode.Space);
+		inputs.Add ("reloadKnop", KeyCode.R);
+		inputs.Add ("forwardKnop", KeyCode.W);
+		inputs.Add ("leftKnop", KeyCode.A);
+		inputs.Add ("downKnop", KeyCode.S);
+		inputs.Add ("rightKnop", KeyCode.D);
 	}
 }
