7728116dda29b89b59cbd87cdcd7f4dadcc85806
jvdwijk <joeyvdwijk@gmail.com>
2016-05-17 16:13:38 +0200

    Pauzescript update

---
+++ Assets/Scripts/PauzeScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauzeScript : MonoBehaviour {
+
+	private InputHandler inputHandler;
+
+	void Awake(){
+		inputHandler = GameObject.FindObjectOfType <InputHandler> ();
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown (inputHandler.inputs["pauzeKnop"]) && Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+		} else if(Input.GetKeyDown (inputHandler.inputs["pauzeKnop"]) && Time.timeScale == 0){
+			Time.timeScale = 1;
+		}
+	}
+}
